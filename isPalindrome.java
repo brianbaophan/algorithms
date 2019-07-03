0f71f286504ed17f5d630a6183bcaee4ce373168
$richbrian$ <brianxphan@gmail.com>
2019-07-03 08:59:30 -0700

    updated palindrome

--- isPalindrome.java
+++ isPalindrome.java
@@ -1,6 +1,23 @@
 class Solution {
     public boolean isPalindrome(String s) {
         
-        return false;
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        
+        int head = 0; 
+        int tail = s.length() - 1;
+        
+        while (head < tail) {
+            
+            if (s.charAt(head) != s.charAt(tail)) 
+                return false;
+            
+            head++;
+            tail--;
+            
+        }
+        
+        return true; 
+        
     }
+    
 }
